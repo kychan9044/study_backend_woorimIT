a73796d7ce3ea58e719d185507ec2d481538befb
kychan9044 <kychan9044@naver.com>
2023-03-09 21:41:51 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// 모듈
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+
+// 라우팅
+const home = require("./routes/home");
+
+// 앱 세팅
+app.set("views", "./views"); // 뷰 파일들이 모여 있는 폴더 지정
+app.set("view engine", "ejs"); // 뷰 엔진을 ejs로 사용
+
+app.use("/", home); // use -> 미들웨어를 등록해주는 메서드.
+
+app.listen(PORT, ()=>{
+    console.log("서버 가동");
+});
