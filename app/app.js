8607316538e576cc2b5054580f46d70583616a1f
kychan9044 <kychan9044@naver.com>
2023-03-13 22:46:29 +0900

    폴더 구조 변경

--- app/app.js
+++ app/app.js
@@ -5,10 +5,10 @@ const express = require("express");
 const app = express();
 
 // 라우팅
-const home = require("./routes/home");
+const home = require("./src/routes/home");
 
 // 앱 세팅
-app.set("views", "./views"); // 뷰 파일들이 모여 있는 폴더 지정
+app.set("views", "./src/views"); // 뷰 파일들이 모여 있는 폴더 지정
 app.set("view engine", "ejs"); // 뷰 엔진을 ejs로 사용
 
 app.use("/", home); // use -> 미들웨어를 등록해주는 메서드.
