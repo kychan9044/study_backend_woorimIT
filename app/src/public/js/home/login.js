b18deb120e72f77c39dcb337df1bbbfc7af5999b
kychan9044 <kychan9044@naver.com>
2023-03-16 21:44:51 +0900

    DOM 이용

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,15 @@
 "use strict";
 
-console.log("hello");
-console.log("bye");
+const id = document.querySelector("#id"),
+    passwd = document.querySelector("#passwd"),
+    loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id: id.value,
+        passwd: passwd.value
+    };
+    console.log(req)
+}
