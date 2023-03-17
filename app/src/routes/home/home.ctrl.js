583b2fa3bf74f3357bf40a44f4afb085876c5545
kychan9044 <kychan9044@naver.com>
2023-03-17 22:19:51 +0900

    로그인 API 기능 추가

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,5 +1,10 @@
 "use strict";
 
+const users = {
+    id: ["kychan"],
+    passwd: ["1234"],
+};
+
 const output = {
     home : (req, res) => {
         res.render("home/index");
@@ -12,7 +17,22 @@ const output = {
 
 const process = {
     login: (req, res) => {
-        console.log(req.body);
+        const id = req.body.id,
+            passwd = req.body.passwd;
+
+        if (users.id.includes(id)){
+            const idx = users.id.indexOf(id);
+            if (users.passwd[idx] == passwd){
+                return res.json({
+                    success: true,
+                })
+            }
+        }
+
+        return res.json({
+            success: false,
+            msg: "로그인에 실패했습니다.",
+        });
     },
 }
 
