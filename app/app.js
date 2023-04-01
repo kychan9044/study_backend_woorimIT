c920971d726884543caf68470f108e22750217b1
kychan9044 <kychan9044@naver.com>
2023-04-01 16:44:16 +0900

    morgan 추가

--- app/app.js
+++ app/app.js
@@ -3,8 +3,12 @@
 // 모듈
 const express = require("express");
 const dotenv = require("dotenv");
-dotenv.config();
+const morgan = require("morgan");
+
 const app = express();
+dotenv.config();
+
+const accessLogStream = require("./src/config/log");
 
 // 라우팅
 const home = require("./src/routes/home");
@@ -13,6 +17,7 @@ const home = require("./src/routes/home");
 app.set("views", "./src/views"); // 뷰 파일들이 모여 있는 폴더 지정
 app.set("view engine", "ejs"); // 뷰 엔진을 ejs로 사용
 app.use(express.static(`${__dirname}/src/public`));
+app.use(morgan("common", {stream: accessLogStream}));
 app.use(express.json());
 // URL으ㄹ 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
 app.use(express.urlencoded({extended: true})); 
